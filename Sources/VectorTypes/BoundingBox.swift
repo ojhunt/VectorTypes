b7b5b983f6663e0b8410b8ffedbbd7d663aafc16
Oliver Hunt <oliver@apple.com>
2022-03-19 17:59:52 -0700

    Bring intersection related stuff back in

--- Sources/VectorTypes/BoundingBox.swift
+++ Sources/VectorTypes/BoundingBox.swift
@@ -68,3 +68,67 @@ public struct BoundingBox<PointType: Point> {
 extension BoundingBox : Sendable where PointType : Sendable {
   
 }
+
+public protocol IntConvertibleFloatingPoint: FloatingPoint, ExpressibleByFloatLiteral {
+  var asInteger: Int { get }
+  init(_: Float)
+  init(_: Double)
+}
+
+extension Float: IntConvertibleFloatingPoint {
+  public var asInteger: Int {
+    return Int(self)
+  }
+}
+
+extension Double: IntConvertibleFloatingPoint {
+  public var asInteger: Int {
+    return Int(self)
+  }
+}
+
+public protocol Ray {
+  associatedtype PointType: Point
+  typealias ValueType = PointType.ValueType
+  var direction: PointType.VectorType { get }
+  var origin: PointType { get }
+}
+
+public extension BoundingBox where PointType: Point, PointType.ValueType: IntConvertibleFloatingPoint, PointType.VectorType: Vector {
+  func offsetRatio(point: PointType, axis: PointType.AxisType) -> PointType.ValueType {
+    let o = point - minBound;
+    let scaleFactor = maxBound[axis] - minBound[axis];
+    return o[axis] / scaleFactor
+  }
+  
+  func surfaceArea() -> PointType.ValueType {
+    let size = maxBound - minBound;
+    var result = PointType.ValueType(1.0)
+    for axis in PointType.AxisType.allAxes {
+      result *= size[axis]
+    }
+    return result;
+  }
+
+  @inline(__always) func intersect<RayType: Ray>(_ ray: RayType, min near: RayType.ValueType, max far: RayType.ValueType) -> (min: RayType.ValueType, max: RayType.ValueType)?
+  where RayType.PointType == PointType {
+    var tmin = PointType.VectorType(repeating: near);
+    var tmax = PointType.VectorType(repeating: far);
+
+    let direction = ray.direction;
+    let origin = ray.origin;
+
+    let inverseDir = PointType.VectorType(repeating: 1.0) ./ direction;
+    let unnormalizedT1 = (minBound - origin) .* inverseDir;
+    let unnormalizedT2 = (maxBound - origin) .* inverseDir;
+    let compareMask = unnormalizedT1 .> unnormalizedT2;
+    let t1 = unnormalizedT1.replace(with: unnormalizedT2, where: compareMask)
+    let t2 = unnormalizedT2.replace(with: unnormalizedT1, where: compareMask);
+    tmin = PointType.VectorType.max(tmin, t1);
+    tmax = PointType.VectorType.min(tmax, t2);
+    if (tmin .> tmax).any {
+      return nil;
+    }
+    return (tmin.maxElement(), tmax.minElement() + 0.01);
+  }
+}
