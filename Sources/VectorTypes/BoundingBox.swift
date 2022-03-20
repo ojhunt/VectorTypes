f41b0457925cbdee46555bd41ca878de2a28303a
Oliver Hunt <oliver@apple.com>
2022-03-19 19:36:30 -0700

    Start the various inline hammers

--- Sources/VectorTypes/BoundingBox.swift
+++ Sources/VectorTypes/BoundingBox.swift
@@ -5,22 +5,22 @@
 //  Created by Oliver Hunt on 3/18/22.
 //
 
-public struct BoundingBox<PointType: Point> {
+@frozen public struct BoundingBox<PointType: Point> {
   typealias AxisType = PointType.AxisType
   
-  public let minBound: PointType;
-  public let maxBound: PointType;
-  public init() {
+  @inline(__always) public let minBound: PointType;
+  @inline(__always) public let maxBound: PointType;
+  @inline(__always) public init() {
     minBound = PointType.max
     maxBound = PointType.min
   }
   
-  public init(min: PointType, max: PointType) {
+  @inline(__always) public init(min: PointType, max: PointType) {
     self.minBound = min
     self.maxBound = max
   }
   
-  public func maxAxis() -> PointType.AxisType {
+  @inline(__always) public func maxAxis() -> PointType.AxisType {
     let range = maxBound - minBound;
     let cases = AxisType.allAxes
     let startIndex = cases.startIndex
@@ -36,19 +36,19 @@ public struct BoundingBox<PointType: Point> {
     return max;
   }
   
-  public func merge(other: Self) -> Self {
+  @inline(__always) public func merge(other: Self) -> Self {
     return BoundingBox(min: PointType.minElements(minBound, other.minBound), max: PointType.maxElements(maxBound, other.maxBound))
   }
   
-  public func merge(point: PointType) -> Self {
+  @inline(__always) public func merge(point: PointType) -> Self {
     return BoundingBox(min: PointType.minElements(minBound, point), max: PointType.maxElements(maxBound, point))
   }
 
-  public func centroid() -> PointType where PointType.VectorType : Vector {
+  @inline(__always) public func centroid() -> PointType where PointType.VectorType : Vector {
     return minBound + (maxBound - minBound) / 2;
   }
   
-  public func surfaceArea() -> PointType.ValueType {
+  @inline(__always) public func surfaceArea() -> PointType.ValueType {
     let size = maxBound - minBound;
     var result = PointType.ValueType(exactly: 1)!
     for axis in PointType.AxisType.allAxes {
@@ -57,7 +57,7 @@ public struct BoundingBox<PointType: Point> {
     return result;
   }
   
-  public func offsetRatio(point: PointType, axis: PointType.AxisType) -> PointType.ValueType
+  @inline(__always) public func offsetRatio(point: PointType, axis: PointType.AxisType) -> PointType.ValueType
   where PointType.ValueType: FloatingPoint {
     let o = point - minBound;
     let scaleFactor = maxBound[axis] - minBound[axis];
@@ -76,13 +76,13 @@ public protocol IntConvertibleFloatingPoint: FloatingPoint, ExpressibleByFloatLi
 }
 
 extension Float: IntConvertibleFloatingPoint {
-  public var asInteger: Int {
+  @inline(__always) public var asInteger: Int {
     return Int(self)
   }
 }
 
 extension Double: IntConvertibleFloatingPoint {
-  public var asInteger: Int {
+  @inline(__always) public var asInteger: Int {
     return Int(self)
   }
 }
@@ -95,13 +95,13 @@ public protocol Ray {
 }
 
 public extension BoundingBox where PointType: Point, PointType.ValueType: IntConvertibleFloatingPoint, PointType.VectorType: Vector {
-  func offsetRatio(point: PointType, axis: PointType.AxisType) -> PointType.ValueType {
+  @inline(__always) func offsetRatio(point: PointType, axis: PointType.AxisType) -> PointType.ValueType {
     let o = point - minBound;
     let scaleFactor = maxBound[axis] - minBound[axis];
     return o[axis] / scaleFactor
   }
   
-  func surfaceArea() -> PointType.ValueType {
+  @inline(__always) func surfaceArea() -> PointType.ValueType {
     let size = maxBound - minBound;
     var result = PointType.ValueType(1.0)
     for axis in PointType.AxisType.allAxes {
